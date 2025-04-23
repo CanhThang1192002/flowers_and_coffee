d92d357c0d29ddefd72aaf62adbca849f20f5dbe
Canh Thang <104011289+CanhThang1192002@users.noreply.github.com>
2025-04-23 09:14:29 +0700

    Initial commit

---
+++ src/routers/index.tsx
@@ -0,0 +1,80 @@
+import { useRoutes } from "react-router-dom";
+import { lazy } from "react";
+import Loadable from "../components/Loadable";
+const Authlayout = Loadable(lazy(() => import("../layouts/authlayout")));
+const MainLayout = Loadable(lazy(() => import("../layouts/mainlayout")));
+const LoginPage = Loadable(lazy(() => import("../pages/authentication/login")));
+const Regiter = Loadable(
+  lazy(() => import("../pages/authentication/register"))
+);
+const HomePage = Loadable(lazy(() => import("../pages/home")));
+const FlowerShopPage = Loadable(lazy(() => import("../pages/flower")));
+const DrinkShopPage = Loadable(lazy(() => import("../pages/drink")));
+const WorkshopPage = Loadable(lazy(() => import("../pages/workshop")));
+const FeedbackPage = Loadable(lazy(() => import("../pages/feedback")));
+const AboutPage = Loadable(lazy(() => import("../pages/about")));
+const ContactForm = Loadable(lazy(() => import("../pages/contact")));
+const BlogPage = Loadable(lazy(() => import("../pages/blog")));
+const BlogPostDetail = Loadable(lazy(() => import("../pages/blog/BlogDetail")));
+
+const AuthenticationRouter = {
+  path: "/auth",
+  element: <Authlayout />,
+  children: [
+    {
+      path: "login",
+      element: <LoginPage />,
+    },
+    {
+      path: "register",
+      element: <Regiter />,
+    },
+  ],
+};
+
+const MainRouter = {
+  path: "/",
+  element: <MainLayout />,
+  children: [
+    {
+      path: "",
+      element: <HomePage />,
+    },
+    {
+      path: "flower",
+      element: <FlowerShopPage />,
+    },
+    {
+      path: "drink",
+      element: <DrinkShopPage />,
+    },
+    {
+      path: "workshop",
+      element: <WorkshopPage />,
+    },
+    {
+      path: "feedback",
+      element: <FeedbackPage />,
+    },
+    {
+      path: "about",
+      element: <AboutPage />,
+    },
+    {
+      path: "contact",
+      element: <ContactForm />,
+    },
+    {
+      path: "blog",
+      element: <BlogPage />,
+    },
+    {
+      path: "blog/detail",
+      element: <BlogPostDetail />,
+    },
+  ],
+};
+
+export default function ThemeRoutes() {
+  return useRoutes([MainRouter, AuthenticationRouter]);
+}
